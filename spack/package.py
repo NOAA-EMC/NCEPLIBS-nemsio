419b5e720bd442a6131df66acc1049ad9cc6b715
Alex Richert <alexander.richert@noaa.gov>
2025-04-17 14:53:43 -0700

    Update package.py

--- spack/package.py
+++ spack/package.py
@@ -46,5 +46,5 @@ class Nemsio(CMakePackage):
         return args
 
     def check(self):
-        with working_dir(self.builder.build_directory):
+        with working_dir(self.build_directory):
             make("test")
